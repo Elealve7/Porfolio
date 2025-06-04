1ec924b6da63dcd655effd760b980c085716758c
Ele117-tech <elena.garalv.4@educa.jcyl.es>
2025-06-04 19:38:56 +0200

    Día 6 - Responsive

--- script.js
+++ script.js
@@ -4,18 +4,7 @@ document.getElementById("proyectos").addEventListener('change', function() {
         window.location.href = ruta;
     }
 });
- // Acordeón toggle
- /*document.querySelector(".acordeon-btn").addEventListener("click", function () {
-    document.querySelector(".acordeon-contenido").classList.toggle("activo");
-  });
 
-  // Carrusel con flechas
-  function moverCarrusel(direccion) {
-    const carrusel = document.getElementById("carrusel-mapas");
-    const imagen = carrusel.querySelector("img");
-    const desplazamiento = imagen.offsetWidth + 20; // ancho + gap
-    carrusel.scrollBy({ left: direccion * desplazamiento, behavior: 'smooth' });
-  } */
     let indiceActual = 0;
 
 function cambiarSlide(direccion) {
