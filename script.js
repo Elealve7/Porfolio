ff504777c2b8b0d629c6f69d2a7ed904db4f2217
Ele117-tech <elena.garalv.4@educa.jcyl.es>
2025-06-04 18:47:14 +0200

    Día 6 - 2.1

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ document.getElementById("proyectos").addEventListener('change', function() {
         window.location.href = ruta;
     }
 });
+ // Acordeón toggle
+ /*document.querySelector(".acordeon-btn").addEventListener("click", function () {
+    document.querySelector(".acordeon-contenido").classList.toggle("activo");
+  });
+
+  // Carrusel con flechas
+  function moverCarrusel(direccion) {
+    const carrusel = document.getElementById("carrusel-mapas");
+    const imagen = carrusel.querySelector("img");
+    const desplazamiento = imagen.offsetWidth + 20; // ancho + gap
+    carrusel.scrollBy({ left: direccion * desplazamiento, behavior: 'smooth' });
+  } */
+    let indiceActual = 0;
+
+function cambiarSlide(direccion) {
+  const carrusel = document.getElementById("sliderTrack");
+  const imagen = carrusel.querySelector("img");
+  const totalSlides = carrusel.children.length;
+
+  indiceActual += direccion;
+
+  if (indiceActual < 0) indiceActual = totalSlides - 1;
+  if (indiceActual >= totalSlides) indiceActual = 0;
+
+  carrusel.style.transform = `translateX(-${indiceActual * imagen.offsetWidth}px)`;
+}
